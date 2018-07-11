c9ab5b15c432961e0a0d6c002ef9525e0d3c0cf7
Austin Thesing <austin@designxdevelop.com>
2018-07-11 12:47:03 -0700

    Random Index/Number Generator
    Built and tested number generator

--- Magic 8-Ball/Magic 8-Ball/ViewController.swift
+++ Magic 8-Ball/Magic 8-Ball/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomBallIndex : Int = 0
+    
+    @IBOutlet weak var ballIAnswerImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func pushedAnswerButton(_ sender: UIButton) {
+        randomBallIndex = Int(arc4random_uniform(5))
+        print(randomBallIndex)
+        
+    }
+    
 }
 
